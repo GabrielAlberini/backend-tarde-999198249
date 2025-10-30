8f64e3f8d0c4c341dc69c725fc482876ed5344a1
gabrielalberini <gabialberini733@gmail.com>
2025-10-30 14:58:08 -0300

    poo, morgan, limit-rate

---
+++ clase-20/backend-utn/src/controllers/productController.ts
@@ -0,0 +1,100 @@
+// FUNCIONES QUE SANITIZAN DATOS DE ENTRADA Y RESPONDEN AL CLIENTE
+// LA REQUEST Y EL RESPONSE SIEMPRE ESTARÁN SOLO EN LOS CONTROLLERS
+
+import { Request, Response } from "express"
+import Product from "../model/ProductModel"
+import { Types } from "mongoose"
+
+const getAllProducts = async (req: Request, res: Response): Promise<void | Response> => {
+  try {
+    const products = await Product.find()
+    res.json({ success: true, data: products })
+  } catch (e) {
+    const error = e as Error
+    res.status(500).json({ success: false, error: error.message })
+  }
+}
+
+const getProduct = async (req: Request, res: Response): Promise<void | Response> => {
+  try {
+    const { id } = req.params
+
+    if (!Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ success: false, error: "ID Inválido" })
+    }
+
+    const product = await Product.findById(id)
+
+    if (!product) {
+      return res.status(404).json({ success: false, error: "Producto no encontrado" })
+    }
+
+    res.status(200).json({ success: true, data: product })
+  } catch (e) {
+    const error = e as Error
+    res.status(500).json({ success: false, error: error.message })
+  }
+}
+
+const addProduct = async (req: Request, res: Response): Promise<void | Response> => {
+  try {
+    const { body } = req
+
+    const { name, description, price, category, stock } = body
+
+    if (!name || !description || !price || !category || !stock) {
+      return res.status(400).json({ message: "Datos invalidos" })
+    }
+
+    const newProduct = new Product({ name, description, price, category, stock })
+
+    await newProduct.save()
+    res.status(201).json(newProduct)
+  } catch (e) {
+    const error = e as Error
+    res.status(500).json({ error: error.message })
+  }
+}
+
+const updateProduct = async (req: Request, res: Response): Promise<void | Response> => {
+  try {
+    const { id } = req.params
+    const { body } = req
+
+    if (!Types.ObjectId.isValid(id)) res.status(400).json({ error: "ID Inválido" })
+
+    const { name, description, price, category, stock } = body
+
+    const updates = { name, description, price, category, stock }
+
+    const product = await Product.findByIdAndUpdate(id, updates, { new: true })
+
+    if (!product) {
+      return res.status(404).json({ error: "Producto no encontrado" })
+    }
+
+    res.json(product)
+  } catch (e) {
+    const error = e as Error
+    res.status(500).json({ error: error.message })
+  }
+}
+
+const deleteProduct = async (req: Request, res: Response): Promise<void | Response> => {
+  try {
+    const id = req.params.id
+
+    if (!Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ error: "ID Inválido" });
+    }
+
+    const product = await Product.findByIdAndDelete(id)
+
+    res.json(product)
+  } catch (e) {
+    const error = e as Error
+    res.status(500).json({ error: error.message })
+  }
+}
+
+export default { getAllProducts, getProduct, addProduct, updateProduct, deleteProduct }
---
+++ clase-20/backend-utn/src/index.ts
@@ -0,0 +1,106 @@
+// LEVANTAR NUESTRO SERIVICIO Y CONFIGURACIONES GLOBALES
+
+// importar herramientas / bibliotecas
+import express, { Request, Response } from "express"
+import { Schema, model, Model } from "mongoose"
+import bcrypt from "bcryptjs"
+import cors from "cors"
+import jwt from "jsonwebtoken"
+import IUser from "./interfaces/IUser"
+import connectDB from "./config/mongodb"
+import productRouter from "./routes/productRoutes"
+import authMiddleware from "./middleware/authMiddleware"
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: any
+    }
+  }
+}
+
+const PORT = 3000
+const SECRET_KEY = "msdjlkfnhasjlkdfasdf"
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+const userSchema = new Schema<IUser>({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+}, {
+  versionKey: false
+})
+
+const User: Model<IUser> = model("User", userSchema)
+
+app.get("/", (request: Request, response: Response) => {
+  response.json({ status: true })
+})
+
+app.post("/auth/register", async (request: Request, response: Response): Promise<void | Response> => {
+  try {
+    const { email, password } = request.body
+
+    if (!email || !password) {
+      return response.status(400).json({ message: "Datos invalidos" })
+    }
+
+    // crear el hash de la contraseña
+    const hash = await bcrypt.hash(password, 10)
+    const newUser = new User({ email, password: hash })
+
+    await newUser.save()
+    response.json(newUser)
+  } catch (e) {
+    const error = e as Error
+    switch (error.name) {
+      case "MongoServerError":
+        return response.status(409).json({ message: "Usuario ya existente en nuestra base de datos" })
+    }
+  }
+})
+
+app.post("/auth/login", async (request: Request, response: Response): Promise<void | Response> => {
+  try {
+    const { email, password } = request.body
+
+    if (!email || !password) {
+      return response.status(400).json({ error: "Datos invalidos" })
+    }
+
+    const user = await User.findOne({ email })
+
+    if (!user) {
+      return response.status(401).json({ error: "No autorizado" })
+    }
+
+    // validar la contraseña
+    const isValid = await bcrypt.compare(password, user.password)
+
+    if (!isValid) {
+      return response.status(401).json({ error: "No autorizado" })
+    }
+
+    const token = jwt.sign({ id: user._id }, SECRET_KEY, { expiresIn: "1h" })
+    response.json({ token })
+  } catch (e) {
+    const error = e as Error
+    response.status(500).json({ error: error.message })
+  }
+})
+
+app.use("/products", productRouter)
+
+// endpoint para el 404 - no se encuentra el recurso
+app.use((request, response) => {
+  response.status(404).json({ error: "El recurso no se encuentra" })
+})
+
+// servidor en escucha
+app.listen(PORT, () => {
+  console.log(`Servidor en escucha en el puerto http://localhost:${PORT}`)
+  connectDB()
+})
